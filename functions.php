5671f9dafbb8671e159e7af612b355d9eaef0860
hawkeye126 <pete.lower@gmail.com>
2013-09-14 13:42:55 -0600

    add google analytics code via functions edit

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 
-add_action('wp_footer', 'add_googleanalytics');
+add_action('wp_head', 'add_googleanalytics');
 function add_googleanalytics() { ?>
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
