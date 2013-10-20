714b80a4620503a62d36c06b4ea46805c45f7a67
kbfoot <kelsey.klora@gmail.com>
2013-10-20 09:15:35 -0500

    Moved the nav out of topbar

--- header.php
+++ header.php
@@ -52,7 +52,18 @@ if ( !defined('ABSPATH')) exit;
                  
        <div id="topBar">  
        
-         <?php get_sidebar('top'); ?>
+    	<ul id='social-media-menu' class='social-media-menu'> 	 
+    		<li><a href="https://www.facebook.com/RubysOfficialpage" target="_blank"><img alt="" src="http://lowermedia.net/ruby-gettinger/assets/sites/23/2013/08/facebook.png"/></a></li>  
+            <li><a href="http://twitter.com/RubyGettinger_" target="_blank"><img alt="" src="http://lowermedia.net/ruby-gettinger/assets/sites/23/2013/08/twitter.png" /></a></li>  
+            <li><a href="http://instagram.com/rubygettinger/" target="_blank"><img alt="" src="http://lowermedia.net/ruby-gettinger/assets/sites/23/2013/08/instagram.png" /></a></li> 		 
+            <li><a href="http://www.youtube.com/writerubygettinger" target="_blank"><img alt="" src="http://lowermedia.net/ruby-gettinger/assets/sites/23/2013/08/youtube.png" /></a></li>  
+            <li><a href="http://pinterest.com/rubygettinger/" target="_blank"><img alt="" src="http://lowermedia.net/ruby-gettinger/assets/sites/23/2013/08/pinterest.png" /></a></li> 	 
+            <li><a href="http://www.whosay.com/rubygettinger" target="_blank"><img alt="" src="http://lowermedia.net/ruby-gettinger/assets/sites/23/2013/08/whosay.png" /></a></li> 	 
+            <li><a href="http://www.linkedin.com/pub/ruby-gettinger/28/4a4/468" target="_blank"><img alt="" src="http://lowermedia.net/ruby-gettinger/assets/sites/23/2013/08/linkedin.png" /></a></li>  
+        </ul> 
+    </div> 
+    
+       <?php get_sidebar('top'); ?>
 				<?php wp_nav_menu(array(
 				    'container'       => 'div',
 						'container_class'	=> 'main-nav',
@@ -71,17 +82,6 @@ if ( !defined('ABSPATH')) exit;
             <?php } ?>
 
 			<?php responsive_header_bottom(); // after header content hook ?>
-       
-    	<ul id='social-media-menu' class='social-media-menu'> 	 
-    		<li><a href="https://www.facebook.com/RubysOfficialpage" target="_blank"><img alt="" src="http://lowermedia.net/ruby-gettinger/assets/sites/23/2013/08/facebook.png"/></a></li>  
-            <li><a href="http://twitter.com/RubyGettinger_" target="_blank"><img alt="" src="http://lowermedia.net/ruby-gettinger/assets/sites/23/2013/08/twitter.png" /></a></li>  
-            <li><a href="http://instagram.com/rubygettinger/" target="_blank"><img alt="" src="http://lowermedia.net/ruby-gettinger/assets/sites/23/2013/08/instagram.png" /></a></li> 		 
-            <li><a href="http://www.youtube.com/writerubygettinger" target="_blank"><img alt="" src="http://lowermedia.net/ruby-gettinger/assets/sites/23/2013/08/youtube.png" /></a></li>  
-            <li><a href="http://pinterest.com/rubygettinger/" target="_blank"><img alt="" src="http://lowermedia.net/ruby-gettinger/assets/sites/23/2013/08/pinterest.png" /></a></li> 	 
-            <li><a href="http://www.whosay.com/rubygettinger" target="_blank"><img alt="" src="http://lowermedia.net/ruby-gettinger/assets/sites/23/2013/08/whosay.png" /></a></li> 	 
-            <li><a href="http://www.linkedin.com/pub/ruby-gettinger/28/4a4/468" target="_blank"><img alt="" src="http://lowermedia.net/ruby-gettinger/assets/sites/23/2013/08/linkedin.png" /></a></li>  
-        </ul> 
-    </div> 
     
 <?php responsive_container(); // before container hook ?>
 <div id="container" class="hfeed">
