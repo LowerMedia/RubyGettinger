cf629b7a73c3e3a482dd63a0deedf7650d4abc57
Pete Lower <pete.lower@gmail.com>
2013-11-20 15:59:58 -0600

    adjusting widget area on homepage

--- blog-excerpt.php
+++ blog-excerpt.php
@@ -22,7 +22,7 @@ if ( !defined('ABSPATH')) exit;
 get_header(); 
 ?>
 
-<?php //if ( function_exists( 'meteor_slideshow' ) ) { meteor_slideshow( "homepage", "" ); } ?>
+<?php if ( function_exists( 'meteor_slideshow' ) ) { meteor_slideshow( "homepage", "" ); } ?>
 <?php get_template_part('homewidget','1'); ?>
 <div id="content-blog" class="<?php echo implode( ' ', responsive_get_content_classes() ); ?>">
         
