1e62e7f51f12c45997e5e95d37262ccf703aa4d2
Pete Lower <pete.lower@gmail.com>
2013-11-20 16:04:53 -0600

    adjusting widget area on homepage

--- homewidget-1.php
+++ homewidget-1.php
@@ -1,5 +1,3 @@
-<?php if ( is_active_sidebar( 'homewidget-1' ) ) : ?>
-	<div class="widget-area homewidget-1" role="complementary">
+<div class="widget-area homewidget-1" role="complementary">
 		<?php dynamic_sidebar( 'homewidget-1' ); ?>
-	</div><!-- .widget-area -->
-<?php endif; ?>
+</div><!-- .widget-area -->
