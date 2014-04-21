5a7922386625738e5e83b35a06c635355169688a
Pete Lower <pete.lower@gmail.com>
2014-04-21 13:19:18 -0500

    adjust slider

--- home.php
+++ home.php
@@ -21,7 +21,7 @@ get_header();
 
 global $more; $more = 0; 
 ?>
-    <?php// if ( function_exists( 'meteor_slideshow' ) ) { meteor_slideshow(); } ?>
+    <?php if ( function_exists( 'meteor_slideshow' ) ) { meteor_slideshow("homepage",""); } ?>
 
 <div id="content-blog" class="<?php echo implode( ' ', responsive_get_content_classes() ); ?>">
         
